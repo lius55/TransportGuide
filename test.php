d53fa8120b7d1588e5d6fdb3d59092e2d6c262e5
lius55 <tony56recruit@gmail.com>
2017-01-13 18:59:58 +0900

    delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$param = substr(strstr($_SERVER["REQUEST_URI"], '?'), 1);
-$url = 'http://api.ekispert.jp/'.$param;
-//echo $url.'<br/>';
-$response = file_get_contents($url);
-echo $response;
-
-?>
